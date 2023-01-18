ed65fcfa0ad299ef2d76049e68210a506479b5a8
Jakub Gert <jakub.gert@zattoo.com>
2023-01-18 14:53:15 +0100

    Renamed package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "VastClient",
+    name: "VastClient3",
     platforms: [
         .iOS(.v13),
         .tvOS(.v13),
@@ -13,8 +13,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "VastClient",
-            targets: ["VastClient"]),
+            name: "VastClient3",
+            targets: ["VastClient3"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,15 +24,16 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "VastClient",
-            dependencies: []),
-        .testTarget(
-            name: "VastClientTests",
-            dependencies: ["VastClient"],
-            resources: [
-                .process("Assets/Vast4-resources"),
-                .process("Assets/Vast3-resources"),
-                .process("Assets/vast3.xsd"),
-                .process("Assets/vast4.xsd"),
-            ]),
+            name: "VastClient3",
+            dependencies: [],
+                path: "Sources/VastClient"),
+//        .testTarget(
+//            name: "VastClientTests",
+//            dependencies: ["VastClient"],
+//            resources: [
+//                .process("Assets/Vast4-resources"),
+//                .process("Assets/Vast3-resources"),
+//                .process("Assets/vast3.xsd"),
+//                .process("Assets/vast4.xsd"),
+//            ]),
     ])
